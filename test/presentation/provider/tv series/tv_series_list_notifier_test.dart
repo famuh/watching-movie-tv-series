8c82e9557824c2043689e33c738a85ffd8345430
Fadhil Muhammad <famuh79@gmail.com>
2022-10-12 10:10:41 +0700

    tv series test notifier

--- test/presentation/provider/tv series/tv_series_list_notifier_test.dart
+++ test/presentation/provider/tv series/tv_series_list_notifier_test.dart
@@ -10,6 +10,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
+import 'tv_series_list_notifier_test.mocks.dart';
+
 
 @GenerateMocks([
   GetNowPlayingTvSeries, 
