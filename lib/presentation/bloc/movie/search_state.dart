fc5ab1b30798a3ca07a2013b3ed47a6f24226efa
Fadhil Muhammad <famuh79@gmail.com>
2022-10-24 14:14:54 +0700

    search for movie BLOC

---
+++ lib/presentation/bloc/movie/search_state.dart
@@ -0,0 +1,40 @@
+part of 'search_bloc.dart';
+
+class SearchState extends Equatable {
+  const SearchState();
+  
+  @override
+  List<Object> get props => [];
+}
+
+class SearchEmpty extends SearchState {}
+
+class SearchLoading extends SearchState {}
+
+class SearchError extends SearchState {
+  final String message;
+
+  SearchError(this.message);
+
+  @override
+  List<Object> get props => [message];
+}
+
+class SearchHasData extends SearchState {
+  final List<Movie> result;
+
+  SearchHasData(this.result);
+
+  @override
+  List<Object> get props => [result];
+
+}
+
+class MovieDetailDataState extends SearchState{
+  final MovieDetail movie;
+
+  MovieDetailDataState(this.movie);
+
+  @override
+  List<Object> get props => [movie];
+}
