6b7083b6037df454d3e15acd93a9b57a3fe49ad6
Fadhil Muhammad <famuh79@gmail.com>
2022-10-27 17:02:15 +0700

    tv tv_series_search_page adjustment changing provider to bloc

--- lib/presentation/pages/tv_series_search_page.dart
+++ lib/presentation/pages/tv_series_search_page.dart
@@ -1,7 +1,9 @@
 import 'package:ditonton/common/constants.dart';
 import 'package:ditonton/common/state_enum.dart';
+import 'package:ditonton/presentation/bloc/tvSeries/tv_series_bloc.dart';
 import 'package:ditonton/presentation/widgets/movie_card_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 
 import '../provider/tv series/tv_series_search_notifier.dart';
@@ -21,10 +23,12 @@ class SearchPageTvSeries extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             TextField(
-              onSubmitted: (query) {
-                Provider.of<TvSeriesSearchNotifier>(context, listen: false)
-                  .fetchTvSeriesSearch(query);
-              },
+              onChanged: (query) {
+                // Provider.of<TvSeriesSearchNotifier>(context, listen: false)
+                //   .fetchTvSeriesSearch(query);
+                  context.read<SearchTvSeriesBloc>().add(OnQueryChangedTvSeries(query)
+                  );
+            },
               decoration: InputDecoration(
                 hintText: 'Search title',
                 prefixIcon: Icon(Icons.search),
@@ -38,22 +42,54 @@ class SearchPageTvSeries extends StatelessWidget {
               style: kHeading6,
             ),
             
-            Consumer<TvSeriesSearchNotifier>(
-              builder: (context, data, child) {
-                if (data.state == RequestState.Loading) {
+            // Consumer<TvSeriesSearchNotifier>(
+            //   builder: (context, data, child) {
+            //     if (data.state == RequestState.Loading) {
+            //       return Center(
+            //         child: CircularProgressIndicator(),
+            //       );
+            //     } else if (data.state == RequestState.Loaded) {
+            //       final result = data.searchResult;
+            //       return Expanded(
+            //         child: ListView.builder(
+            //           padding: const EdgeInsets.all(8),
+            //           itemBuilder: (context, index) {
+            //             final tvSeries = data.searchResult[index];
+            //             return TvSeriesCard(tvSeries);
+            //           },
+            //           itemCount: result.length,
+            //         ),
+            //       );
+            //     } else {
+            //       return Expanded(
+            //         child: Container(),
+            //       );
+            //     }
+            //   },
+            // ),
+          
+          BlocBuilder<SearchTvSeriesBloc, SearchStateTvSeries>(
+              builder: (context, state) {
+                if (state is SearchLoading) {
                   return Center(
                     child: CircularProgressIndicator(),
                   );
-                } else if (data.state == RequestState.Loaded) {
-                  final result = data.searchResult;
+                } else if (state is SearchTvSeriesHasData) {
+                  final result = state.result;
                   return Expanded(
                     child: ListView.builder(
                       padding: const EdgeInsets.all(8),
                       itemBuilder: (context, index) {
-                        final tvSeries = data.searchResult[index];
+                        final tvSeries = result[index];
                         return TvSeriesCard(tvSeries);
                       },
                       itemCount: result.length,
+                    ),
+                  );
+                } else if (state is SearchError) {
+                  return Expanded(
+                    child: Center(
+                      child: Text(state.message),
                     ),
                   );
                 } else {
