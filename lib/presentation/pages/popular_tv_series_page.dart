5d2a7118c9e2ec4a18848499a592d33264f09e86
Fadhil Muhammad <famuh79@gmail.com>
2022-10-28 01:22:39 +0700

    popular_tv_series_page adjustment changing provider to bloc

--- lib/presentation/pages/popular_tv_series_page.dart
+++ lib/presentation/pages/popular_tv_series_page.dart
@@ -1,6 +1,8 @@
 import 'package:ditonton/common/state_enum.dart';
+import 'package:ditonton/presentation/bloc/tvSeries/tv_series_bloc.dart';
 import 'package:ditonton/presentation/widgets/movie_card_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 
 import '../provider/tv series/popular_tv_series_notifier.dart';
@@ -17,7 +19,9 @@ class _PopularTvSeriesPageState extends State<PopularTvSeriesPage> {
   void initState() {
     super.initState();
     Future.microtask(() =>
-        Provider.of<PopularTvSeriesNotifier>(context, listen: false).fetchPopularTvSeries());
+        // Provider.of<PopularTvSeriesNotifier>(context, listen: false).fetchPopularTvSeries());
+        context.read<PopularTvSeriesBloc>().add(FetchPopularTvSeries())
+    );
   }
 
   @override
@@ -28,29 +32,53 @@ class _PopularTvSeriesPageState extends State<PopularTvSeriesPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Consumer<PopularTvSeriesNotifier>(
-          builder: (context, data, child) {
-            if (data.state == RequestState.Loading) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            } else if (data.state == RequestState.Loaded) {
-              return ListView.builder(
-                itemBuilder: (context, index) {
-                  final tvSeries = data.tvSeries[index];
-                  return TvSeriesCard(tvSeries);
-                },
-                itemCount: data.tvSeries.length,
-              );
-            } else {
-              return Center(
-                key: Key('error_message'),
-                child: Text(data.message),
-              );
+        child: BlocBuilder<PopularTvSeriesBloc, TvSeriesStateBloc>(
+            builder: (context, state) {
+              if (state is TvSeriesLoading) {
+                return Center(child: CircularProgressIndicator());
+              } else if (state is TvSeriesHasData) {
+                return ListView.builder(
+                  itemBuilder: (context, index) {
+                    final tvSeries = state.tvSeries[index];
+                    return TvSeriesCard(tvSeries);
+                  },
+                  itemCount: state.tvSeries.length,
+                );
+              } else if (state is TvSeriesError) {
+                return Center(
+                  child: Text(state.message),
+                );
+              } else {
+                return Center(
+                  child: Text('No Movies :('),
+                );
+              }
             }
-          },
-        ),
+
+
+        // Consumer<PopularTvSeriesNotifier>(
+        //   builder: (context, data, child) {
+        //     if (data.state == RequestState.Loading) {
+        //       return Center(
+        //         child: CircularProgressIndicator(),
+        //       );
+        //     } else if (data.state == RequestState.Loaded) {
+        //       return ListView.builder(
+        //         itemBuilder: (context, index) {
+        //           final tvSeries = data.tvSeries[index];
+        //           return TvSeriesCard(tvSeries);
+        //         },
+        //         itemCount: data.tvSeries.length,
+        //       );
+        //     } else {
+        //       return Center(
+        //         key: Key('error_message'),
+        //         child: Text(data.message),
+        //       );
+        //     }
+        //   },
+        // ),
       ),
-    );
+    ));
   }
 }
